32a5498c33d22360dab123653bf69d41155aa6a4
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-10-09 16:01:24 -0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import {projectAuth} from './firebase/config'
 import Home from './components/Home'
 import Login from './components/Login'
 
-
-
 function App() {
 
   const [user, setUser] = useState(null)
