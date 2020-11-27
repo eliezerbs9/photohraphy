baf745d854fbfa49253327e0a8411d0a02cf7acd
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-11-27 15:04:09 -0800

    Fixed problem with duplicate files names

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -9,14 +9,28 @@ const useStorage = (file) => {
     useEffect(() => {
         const storageRef = storage.ref(file.name)
 
-        storageRef.put(file).on('state_changed', (snap) => {
-            let percentage = (snap.bytesTransferred / snap.totalBytes ) * 100
-            setProgress(percentage)
-        }, (err) => {
-            setError(err)
-        }, async () => {
-            const url = await storageRef.getDownloadURL()
-            setUrl(url)
+        storageRef.getDownloadURL().then((url) => {
+            //CHECK IF ALREADY EXISTS A FILE WITH THAT SAME NAME 
+            if(url){
+                console.log('File with the same name already exists: ', url)
+                //IF YES SET THE URL TO THA FILE'S URL AND PROGRESS 100% (NO FILE WAS UPLOADED THO)
+                setUrl(url)
+                setProgress(100)
+            }else{
+                //IF NOT, UPLOAD THAT FILE TO FIRESTIRE
+                storageRef.put(file).on('state_changed', (snap) => {
+                    //UPDATE PROGRESS BAR WITH A REAL TIME SNAP 
+                    let percentage = (snap.bytesTransferred / snap.totalBytes ) * 100
+                    setProgress(percentage)
+                }, (err) => {
+                    setError(err)
+                }, async () => {
+                    //WHEN ITS DONE RETURNS A URL OF THE UPLOADED FILE
+                    const url = await storageRef.getDownloadURL()
+                    console.log('getDownloadURL at the end: ', url)
+                    setUrl(url)
+                })
+            }
         })
     },[file])
 
