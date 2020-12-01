3b230e8c13430e1c465220b47462ec3f8741b83f
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-12-01 15:42:05 -0800

    created callback function to be called when user closes modal

--- src/components/Layout/Modal.js
+++ src/components/Layout/Modal.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
-const Modal = ({children, visible, setVisible}) => {
+const Modal = ({children, visible, setVisible, closeCallback}) => {
 
     const modal = document.getElementById('backdrop');
 
     const closeModal = (e) => {
         if(e.target.classList.contains('backdrop')){
+            console.log('closeModal')
             setVisible(false);
+            if(closeCallback){
+                closeCallback()
+            }
         }
     }
 
+    const closeStyle = {
+        cursor: 'pointer',
+        color: 'var(--primary)',
+        fontWeight: '900',
+        right: '10px',
+        position: 'absolute'
+    }
     return (
         <div 
             id="backdrop"
--- src/components/Photos/AddPhotoModal.js
+++ src/components/Photos/AddPhotoModal.js
@@ -7,12 +7,16 @@ const AddPhotoModal = ({visible, setVisible, album}) => {
     const [files, setFiles] = useState([]);
     const [errors, setErrors] = useState([])
 
+    const resetState = () => {
+        setFiles([])
+        setErrors([])
+    }
+
     return (
         <>
-            <Modal visible={visible} setVisible={setVisible}>
+            <Modal visible={visible} setVisible={setVisible} closeCallback={resetState}>
                 <div className="modal_form">
                     <AddPhoto album={album} files={files} setFiles={setFiles} errors={errors} setErrors={setErrors}/>
-                    <button className="btn btn--lg btn--primary" style={{margin: '0'}} onClick={() => {setFiles([]); setVisible(false)}}>Done</button>
                 </div>
             </Modal>
         </>
